3391f55559a3bc3dbcdb70d5ed910999cb946ed5
Taylor Eke <tayloreke@gmail.com>
2023-06-12 12:45:33 -0700

    Stop sending transactions to taxjar
    Signed-off-by: Taylor Eke <tayloreke@gmail.com>

--- app/models/spree_taxjar/spree/order_decorator.rb
+++ app/models/spree_taxjar/spree/order_decorator.rb
@@ -4,7 +4,9 @@ module SpreeTaxjar
       def self.prepended(base)
         base.include Taxable
 
-        base.state_machine.after_transition to: :complete, do: :capture_taxjar
+        # SW-5132 error from taxjar "amount not equal to line items and shipping" because we sometimes make
+        # whole order adjustments that cannot be reconciled to any specific line items or shipping.
+        # base.state_machine.after_transition to: :complete, do: :capture_taxjar
         base.state_machine.after_transition to: :canceled, do: :delete_taxjar_transaction
         base.state_machine.after_transition to: :resumed, from: :canceled, do: :capture_taxjar
       end
