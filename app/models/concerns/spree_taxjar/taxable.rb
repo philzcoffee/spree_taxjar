4729bcd2f251c8189dc96c0ee7d952c886342f62
Joe Swatosh <7006129+swatosh@users.noreply.github.com>
2024-03-18 16:16:25 -0700

    Update taxable.rb
    Signed-off-by: Joe Swatosh <7006129+swatosh@users.noreply.github.com>

--- app/models/concerns/spree_taxjar/taxable.rb
+++ app/models/concerns/spree_taxjar/taxable.rb
@@ -5,7 +5,7 @@ module SpreeTaxjar
     private
 
     def taxjar_applicable?(order)
-      return false if order.state = 'cart'
+      return false if order.state == 'cart'
 
       ::Spree::TaxRate.match(order.tax_zone).any? { |rate| rate.calculator_type == "Spree::Calculator::TaxjarCalculator" }
     end
