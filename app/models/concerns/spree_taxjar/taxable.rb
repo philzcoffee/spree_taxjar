791469318471d4f3f6bed57d7ca16e2e24bb0914
Joe Swatosh <7006129+swatosh@users.noreply.github.com>
2024-03-18 16:12:29 -0700

    Update taxable.rb
    make nothing in the "cart" state taxable
    
    Signed-off-by: Joe Swatosh <7006129+swatosh@users.noreply.github.com>

--- app/models/concerns/spree_taxjar/taxable.rb
+++ app/models/concerns/spree_taxjar/taxable.rb
@@ -5,6 +5,8 @@ module SpreeTaxjar
     private
 
     def taxjar_applicable?(order)
+      return false if order.state = 'cart'
+
       ::Spree::TaxRate.match(order.tax_zone).any? { |rate| rate.calculator_type == "Spree::Calculator::TaxjarCalculator" }
     end
   end
